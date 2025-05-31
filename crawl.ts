e0f33ebfbc83a39f5dacc1a4edfd75ac6b7edfdb
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-05-31 21:09:00 +0900

    クローラーのURLリストを有効化し、ヘッドレスモードを無効にしました。

--- crawl.ts
+++ crawl.ts
@@ -33,7 +33,6 @@ class CrawlingUsecase {
 async function main() {
 	console.log("Launching browser...");
 	const browser = await chromium.launch({
-		headless: false,
 		timeout: 30000,
 	});
 
@@ -41,13 +40,13 @@ async function main() {
 		const coconalaCrawlUsecase = new CrawlingUsecase(
 			new CoconalaCrawler(browser),
 			[
-				// "https://coconala.com/requests/categories/231?ref=header&categoryId=231&page=1&recruiting=true",
-				// "https://coconala.com/requests/categories/230?ref=header&categoryId=230&page=1&recruiting=true",
-				// "https://coconala.com/requests/categories/232?ref=header&categoryId=232&page=1&recruiting=true",
-				// "https://coconala.com/requests/categories/237?ref=header&categoryId=237&page=1&recruiting=true",
-				// "https://coconala.com/requests/categories/813?ref=header&categoryId=813&page=1&recruiting=true",
-				// "https://coconala.com/requests/categories/239?ref=header&categoryId=239&page=1&recruiting=true",
-				// "https://coconala.com/requests/categories/236?ref=header&categoryId=236&page=1&recruiting=true",
+				"https://coconala.com/requests/categories/231?ref=header&categoryId=231&page=1&recruiting=true",
+				"https://coconala.com/requests/categories/230?ref=header&categoryId=230&page=1&recruiting=true",
+				"https://coconala.com/requests/categories/232?ref=header&categoryId=232&page=1&recruiting=true",
+				"https://coconala.com/requests/categories/237?ref=header&categoryId=237&page=1&recruiting=true",
+				"https://coconala.com/requests/categories/813?ref=header&categoryId=813&page=1&recruiting=true",
+				"https://coconala.com/requests/categories/239?ref=header&categoryId=239&page=1&recruiting=true",
+				"https://coconala.com/requests/categories/236?ref=header&categoryId=236&page=1&recruiting=true",
 			],
 			{
 				saveMany: async (projects: Project[]) => {
@@ -65,16 +64,16 @@ async function main() {
 			new CrowdWorksCrawler(browser),
 			[
 				"https://crowdworks.jp/public/jobs/search?category_id=2&order=new",
-				// "https://crowdworks.jp/public/jobs/search?category_id=2&order=new&page=2",
-				// "https://crowdworks.jp/public/jobs/search?category_id=83&order=new",
-				// "https://crowdworks.jp/public/jobs/search?category_id=83&order=new&page=2",
-				// "https://crowdworks.jp/public/jobs/search?category_id=282&order=new",
-				// "https://crowdworks.jp/public/jobs/search?category_id=173&order=new",
-				// "https://crowdworks.jp/public/jobs/search?category_id=78&order=new",
-				// "https://crowdworks.jp/public/jobs/search?category_id=346&order=new",
-				// "https://crowdworks.jp/public/jobs/search?category_id=347&order=new",
-				// "https://crowdworks.jp/public/jobs/search?category_id=348&order=new",
-				// "https://crowdworks.jp/public/jobs/search?category_id=269&order=new",
+				"https://crowdworks.jp/public/jobs/search?category_id=2&order=new&page=2",
+				"https://crowdworks.jp/public/jobs/search?category_id=83&order=new",
+				"https://crowdworks.jp/public/jobs/search?category_id=83&order=new&page=2",
+				"https://crowdworks.jp/public/jobs/search?category_id=282&order=new",
+				"https://crowdworks.jp/public/jobs/search?category_id=173&order=new",
+				"https://crowdworks.jp/public/jobs/search?category_id=78&order=new",
+				"https://crowdworks.jp/public/jobs/search?category_id=346&order=new",
+				"https://crowdworks.jp/public/jobs/search?category_id=347&order=new",
+				"https://crowdworks.jp/public/jobs/search?category_id=348&order=new",
+				"https://crowdworks.jp/public/jobs/search?category_id=269&order=new",
 			],
 			{
 				saveMany: async (projects: Project[]) => {
